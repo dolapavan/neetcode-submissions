a73b630b1d73f109ef36116f43ad7682db34b750
dolapavan <101808908+dolapavan@users.noreply.github.com>
2026-08-09 18:21:04 +0530

    Bulk sync: 8 submissions

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>> m=new HashMap<>();
+        for(int i=0;i<strs.length;i++){
+            char[] c=strs[i].toCharArray();
+            Arrays.sort(c);
+            String s=new String(c);
+
+            m.putIfAbsent(s,new ArrayList<>());
+            m.get(s).add(strs[i]);
+
+        }
+        return new ArrayList<>(m.values());
+    }
+}
