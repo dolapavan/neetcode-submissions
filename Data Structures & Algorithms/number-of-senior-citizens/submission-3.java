f9e3ac981dd17c1e103c4cfc51bec66825345885
dolapavan <101808908+dolapavan@users.noreply.github.com>
2026-08-23 13:51:42 +0530

    Add: number-of-senior-citizens - submission-3

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-3.java
@@ -0,0 +1,33 @@
+// class Solution {
+//     public int countSeniors(String[] details) {
+
+//         int count=0;
+//         for(String s:details){
+//             int i=10;
+//             int age=Integer.parseInt(s.substring(i+1,i+3));
+//             if(age>60){
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// }
+
+// or 
+
+class Solution {
+    public int countSeniors(String[] details) {
+
+        int count=0;
+        for(String s:details){
+            int i=10;
+            if(s.charAt(i) == 'M' || s.charAt(i) == 'F' || s.charAt(i) == 'O'){
+                int age=Integer.parseInt(s.substring(i+1,i+3));
+                if(age>60){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
